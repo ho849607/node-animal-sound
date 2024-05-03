0e4eacfb3bf08bce83f6a08d1aa2b56686190951
ho849607 <127944369+ho849607@users.noreply.github.com>
2024-05-03 21:03:38 +0900

    조코딩선생님과 함께하는 수업 (first commit)

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/sound/:name', (req, res) => {
+    const { name } = req.params;
+    let sound;
+    if (name == "dog") {
+        sound = "멍멍";
+    } else if (name == "cat") {
+        sound = '야옹';
+    } else if (name == "pig") {
+        sound = '꿀꿀';
+    } else {
+        sound = '알 수 없음';
+    }
+    res.json({ sound });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
